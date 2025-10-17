29c5db4f9793e2b1f837eb235a1f5d28ba240479
Iulian565 <iulian.marginean04@e-uvt.ro>
2025-10-17 13:39:41 +0300

    Added AlignStrategy pattern with AlignLeft and AlignRight implementations

--- src/Paragraph.java
+++ src/Paragraph.java
@@ -1,12 +1,21 @@
 public class Paragraph implements Element {
     private String text;
+    private AlignStrategy alignStrategy; // Strategy pattern
 
     public Paragraph(String text) {
         this.text = text;
     }
 
+    public void setAlignStrategy(AlignStrategy strategy) {
+        this.alignStrategy = strategy;
+    }
+
     @Override
     public void print() {
-        System.out.println("Paragraph: " + text);
+        if (alignStrategy == null) {
+            System.out.println("Paragraph: " + text);
+        } else {
+            alignStrategy.render(text);
+        }
     }
 }
