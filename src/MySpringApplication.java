2ba161f900ee689b8e8c95138caca447fff10871
Iulian565 <iulian.marginean04@e-uvt.ro>
2025-11-24 23:22:27 +0200

    Implementare - Dependency Injection + Controller

--- src/MySpringApplication.java
+++ src/MySpringApplication.java
@@ -16,11 +16,9 @@ public class MySpringApplication {
 
     public static void main(String[] args) {
 
-        // Pornim contextul DI
         ApplicationContext context =
                 SpringApplication.run(MySpringApplication.class, args);
 
-        // 1. Testăm bean-ul PROTOTYPE
         TransientComponent transientBean =
                 context.getBean(TransientComponent.class);
         transientBean.operation();
@@ -28,7 +26,6 @@ public class MySpringApplication {
         transientBean = context.getBean(TransientComponent.class);
         transientBean.operation();
 
-        // 2. Testăm bean-ul SINGLETON
         SingletonComponent singletonBean =
                 context.getBean(SingletonComponent.class);
         singletonBean.operation();
@@ -36,11 +33,9 @@ public class MySpringApplication {
         singletonBean = context.getBean(SingletonComponent.class);
         singletonBean.operation();
 
-        // 3. Testăm ClientComponent
         ClientComponent c = context.getBean(ClientComponent.class);
         c.operation();
 
-        // 4. Obținem același bean după nume
         c = (ClientComponent) context.getBean("clientComponent");
         c.operation();
     }
